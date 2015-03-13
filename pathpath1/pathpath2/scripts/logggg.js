c29cdfcc0cd79e1c914a422ef281cf5a01a89a3b
pbaffiliate10 <pbaffiliate10@gmail.com>
2015-03-14 00:24:04 +0200

    sdf

---
+++ pathpath1/pathpath2/scripts/logggg.js
@@ -0,0 +1,11 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=anonymous 
+  **/ 
+ console.error("fgdfgfdgdfg");
+console.info("fgdfgfdgdfg");
+console.warn("fgdfgfdgdfg");
+console.debug("fgdfgfdgdfg");
+console.log("fgdfgfdgdfg");
+return "test loggingf";   							
