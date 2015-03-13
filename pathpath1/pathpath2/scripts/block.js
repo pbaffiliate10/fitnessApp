5cb1e16189a78b1ac8d942c447edb43d2ffbd1b3
pbaffiliate10 <pbaffiliate10@gmail.com>
2015-03-14 00:34:59 +0200

    Update block.js

--- pathpath1/pathpath2/scripts/block.js
+++ pathpath1/pathpath2/scripts/block.js
@@ -4,4 +4,4 @@ write=nobody
 execute=authenticated 
   **/ 
  /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block id="26" type="scriptr_return" x="466" y="29" inline="false"><value name="return"><block id="18" type="text"><field name="TEXT">test1111</field></block></value></block></xml>*#*#*/
-return 'test1111'   							
+return 'zakhiaaa'   							
