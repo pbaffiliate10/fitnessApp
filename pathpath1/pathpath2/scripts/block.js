dc46361bc79cfaa6b6444caad7640eb35651f9c5
pbaffiliate10 <pbaffiliate10@gmail.com>
2015-03-14 00:33:27 +0200

    s

---
+++ pathpath1/pathpath2/scripts/block.js
@@ -0,0 +1,7 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block id="26" type="scriptr_return" x="466" y="29" inline="false"><value name="return"><block id="18" type="text"><field name="TEXT">test1111</field></block></value></block></xml>*#*#*/
+return 'test1111'   							
